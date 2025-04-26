8f1bbf0c6e15885936b819016c365b495c3220d5
Gavin Hudman <hudman.gavin@gmail.com>
2025-04-26 15:39:40 -0400

    added ids for buttons

---
+++ script.js
@@ -0,0 +1,33 @@
+function add(a, b){
+    return a + b;
+};
+
+function subtract(a, b){
+    return a - b;
+};
+
+function multiply(a, b){
+    return a * b;
+};
+
+function divide(a, b){
+    return a / b;
+};
+
+var firstNumber = '';
+var secondNumber = '';
+var operator = '';
+
+function operate (firstNumber, operator, secondNumber){
+    if (operator = '+'){
+        return add(firstNumber, secondNumber);
+    } if (operator = '-'){
+        return subtract(firstNumber, secondNumber);
+    } if (operator = '*'){
+        return multiply(firstNumber, secondNumber);
+    } if (operator = '/'){
+        return divide(firstNumber, secondNumber);
+    }
+};
+
+const calcDisplay = document.querySelector(".display");
