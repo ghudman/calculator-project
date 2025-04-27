8a3612843ce2deea24e2e0658c3bd55f2e53c9ad
Gavin Hudman <hudman.gavin@gmail.com>
2025-04-27 12:19:49 -0400

    Changes up until problem with second operation carrying over first result: fix is in operatorButton function.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function add(a, b){
-    return a + b;
+function add(firstNumber, secondNumber){
+    return Number(firstNumber) + Number(secondNumber);
 };
 
-function subtract(a, b){
-    return a - b;
+function subtract(firstNumber, secondNumber){
+    return firstNumber - secondNumber;
 };
 
-function multiply(a, b){
-    return a * b;
+function multiply(firstNumber, secondNumber){
+    return firstNumber * secondNumber;
 };
 
-function divide(a, b){
-    return a / b;
+function divide(firstNumber, secondNumber){
+    return firstNumber / secondNumber;
 };
 
 var firstNumber = '';
@@ -19,16 +19,74 @@ var secondNumber = '';
 var operator = '';
 
 function operate (firstNumber, operator, secondNumber){
-    if (operator = '+'){
+    if (operator === '+'){
         return add(firstNumber, secondNumber);
-    } if (operator = '-'){
+    } if (operator === '-'){
         return subtract(firstNumber, secondNumber);
-    } if (operator = '*'){
+    } if (operator === '*'){
         return multiply(firstNumber, secondNumber);
-    } if (operator = '/'){
+    } if (operator === '/'){
         return divide(firstNumber, secondNumber);
     }
 };
 
 const calcDisplay = document.querySelector(".display");
-const buttons = document.querySelector("keypad");
+const numbers = document.querySelector(".numbers");
+const operatorButtons = document.querySelector(".operends");
+const clearButton = document.querySelector('#clear');
+var selectedNum = [];
+
+//Creates the number used
+numbers.addEventListener('click', function(buttonClick){
+    if (buttonClick.target.tagName === 'BUTTON'){
+        selectedNum.push(buttonClick.target.textContent);
+        calcDisplay.textContent = selectedNum.join('');
+    }
+});
+
+//Operator and clearing of selection
+operatorButtons.addEventListener('click', function(op){
+    if (op.target.tagName === 'BUTTON') {
+        operator = op.target.textContent; //sets operator
+        firstNumber = selectedNum.join(""); //creates string from array for the first number
+        selectedNum = []; //clears the selection for next number
+        console.log(firstNumber);
+        console.log(selectedNum);
+    }
+});
+
+const equalsButton = document.querySelector('#equals');
+
+equalsButton.addEventListener('click', function(){
+    secondNumber = selectedNum.join("");
+    if (firstNumber === ""){
+        calcDisplay.textContent = "";
+    }
+    if (secondNumber == '0'){
+        calcDisplay.textContent = "What did you think was going to happen?"
+    } else {
+        var result = operate(firstNumber, operator, secondNumber);
+
+        calcDisplay.textContent = result;
+        selectedNum = [];
+        firstNumber = result;
+        secondNumber = '';
+        operator = '';
+        result = '';
+        console.log(selectedNum);
+        console.log(firstNumber);
+        console.log(secondNumber);
+        console.log(operator);
+        console.log(result);
+        
+    }
+});
+
+//clears calculator
+clearButton.addEventListener('click', function(){
+    calcDisplay.textContent = ''; //clears display
+    selectedNum = []; // clears selected number
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+});
